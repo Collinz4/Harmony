124a515b9056333ccd4b54f714aa06f913e493f3
Stephen Collins <stephen436901@gmail.com>
2020-12-29 02:39:29 -0500

    Removed conf file.

--- Collector/conf.py
+++
@@ -1,7 +0,0 @@
-import os
-
-SERVER_ADDRESS: str = os.getenv('SERVER_ADDRESS')
-SERVER_PORT: str = int(os.getenv('SERVER_PORT') or -1)
-SERVER_DEBUG_MODE: bool = bool(os.getenv('SERVER_DEBUG_MODE') == 'True')
-
-API_KEY: str = os.getenv('API_KEY')
